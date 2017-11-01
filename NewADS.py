78eae8922a68513a00bf0b52e24d69f1fb12f90c
Kanishk Sharan <32040557+kanishksharan12@users.noreply.github.com>
2017-10-31 17:43:31 -0700

    Add files via upload

---
+++ NewADS.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+__author__ = 'Kanishk Sharan'
+
+
+#  this requires NTFS on the file system
+fh = open("file.txt:myfile", "w")
+fh.write("this is a test")
+fh.close()
+
+fh = open("file.txt:myfile", "r")
+data = fh.read(100)
+print(data)
+
+
